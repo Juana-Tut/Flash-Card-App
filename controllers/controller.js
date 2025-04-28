5084dd79dc674883928811133503cfc1c57b2a20
Juana Tut <2023159062@ub.edu.bz>
2025-04-27 21:49:19 -0600

    Added controller to update and delete a flashcard

--- controllers/controller.js
+++ controllers/controller.js
@@ -38,3 +38,28 @@ export const getFlashCards = async (req, res) => {
         res.status(500).send('Error retrieving flashcards');
     }
 };
+
+export const updateFlashCard = async (req, res) => {
+    const { id } = req.params;
+    const { front, back } = req.body;
+    try {
+        const query = 'UPDATE flashcards SET front = $1, back = $2 WHERE id = $3';
+        await pool.query(query, [front, back, id]);
+        res.status(200).redirect('/api/flashcards/view');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error updating flashcard');
+    }
+};
+
+export const deleteFlashCard = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const query = 'DELETE FROM flashcards WHERE id = $1';
+        await pool.query(query, [id]);
+        res.status(200).redirect('/api/flashcards/view');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error deleting flashcard');
+    }
+};
